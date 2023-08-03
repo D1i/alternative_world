bd18163a2f966bd9f85e852a9ee185a9c59e5a47
K1ngVlad <kingvlad3008@gmail.com>
2023-08-03 14:29:54 +0400

    audio

---
+++ src/core/audio/AudioCore.tsx
@@ -0,0 +1,13 @@
+export function AudioCore() {
+  class asadsad {
+    soundId: number;
+    volume: number;
+
+    constructor(soundId) {
+      this.soundId = soundId;
+      this.volume = 0.5;
+    }
+  }
+
+  return <></>;
+}
